06a01fbe2af1cc2052605c2147ff55bd4230d787
Dmitrii Belousov <dmitriilir2697@gmail.com>
2018-08-07 17:48:04 +1000

    'dz4Finish'

--- src/index.js
+++ src/index.js
@@ -24,7 +24,8 @@ function createDivWithText(text) {
  Функция должна вставлять элемент, переданный в переметре what в начало элемента, переданного в параметре where
 
  Пример:
-   prepend(document.querySelector('#one'), document.querySelector('#two')) // добавит элемент переданный первым аргументом в начало элемента переданного вторым аргументом
+   prepend(document.querySelector('#one'), document.querySelector('#two')) // добавит элемент переданный первым
+   аргументом в начало элемента переданного вторым аргументом
  */
 function prepend(what, where) {
     where.prepend(what);
@@ -35,7 +36,8 @@ function prepend(what, where) {
 
  3.1: Функция должна перебрать все дочерние элементы узла, переданного в параметре where
 
- 3.2: Функция должна вернуть массив, состоящий из тех дочерних элементов следующим соседом которых является элемент с тегом P
+ 3.2: Функция должна вернуть массив, состоящий из тех дочерних элементов следующим соседом которых является элемент с
+ тегом P
 
  Пример:
    Представим, что есть разметка:
@@ -47,7 +49,8 @@ function prepend(what, where) {
       <p></p>
    </dody>
 
-   findAllPSiblings(document.body) // функция должна вернуть массив с элементами div и span т.к. следующим соседом этих элементов является элемент с тегом P
+   findAllPSiblings(document.body) // функция должна вернуть массив с элементами div и span т.к. следующим соседом этих
+    элементов является элемент с тегом P
 */
 function findAllPSiblings(where) {
     var childList = where.children,
@@ -65,7 +68,9 @@ function findAllPSiblings(where) {
 /*
  Задание 4:
 
- Функция представленная ниже, перебирает все дочерние узлы типа "элемент" внутри узла переданного в параметре where и возвращает массив из текстового содержимого найденных элементов
+ Функция представленная ниже, перебирает все дочерние узлы типа "элемент" внутри узла переданного в параметре where и
+ возвращает массив из текстового содержимого найденных элементов
+
  Но похоже, что в код функции закралась ошибка и она работает не так, как описано.
 
  Необходимо найти и исправить ошибку в коде так, чтобы функция работала так, как описано выше.
@@ -104,18 +109,20 @@ function findError(where) {
 function deleteTextNodes(where) {
     var childList = where.childNodes;
 
-    for(let i = 0; childList.length > i; i++){
-        if(childList[i].nodeType === 3){
+    for (let i = 0; childList.length > i; i++) {
+
+        if (childList[i].nodeType === 3) {
             where.removeChild(childList[i]);
         }
+
     }
 }
 
 /*
  Задание 6:
 
- Выполнить предудыщее задание с использование рекурсии - то есть необходимо заходить внутрь каждого дочернего элемента (углубляться в дерево)
-
+Выполнить предудыщее задание с использование рекурсии - то есть необходимо заходить внутрь каждого дочернего элемента
+(углубляться в дерево)
 
  Пример:
    После выполнения функции, дерево <span> <div> <b>привет</b> </div> <p>loftchool</p> !!!</span>
@@ -123,7 +130,8 @@ function deleteTextNodes(where) {
  */
 function deleteTextNodesRecursive(where) {
     var childList = where.childNodes;
-    for (let i = 0;  childList.length > i; i++) {
+
+    for (let i = 0; childList.length > i; i++) {
         let child = childList[i];
 
         if (child.nodeType === 3) {
@@ -155,6 +163,7 @@ function deleteTextNodesRecursive(where) {
      texts: 3
    }
  */
+
 function collectDOMStat(root) {
 }
 
