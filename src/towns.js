7ca5370a2b27155281372565e86fdd0335f33063
Dmitrii Belousov <dmitriilir2697@gmail.com>
2018-08-15 17:00:16 +1000

    'dz6Complete'

--- src/towns.js
+++ src/towns.js
@@ -35,28 +35,30 @@
  Массив городов пожно получить отправив асинхронный запрос по адресу
  https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
  */
-function loadTowns() {
-    return new Promise((resolve) => {
+async function loadTowns() {
+    return new Promise((resolve, reject) => {
             const xhr = new XMLHttpRequest();
             xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
             xhr.responseType = 'json';
             xhr.send();
             xhr.addEventListener('load', () => {
-                resolve(xhr.response.sort(function (a, b) {
-                    if (a.name > b.name) {
-                        return 1;
-                    }
-                    if (a.name < b.name) {
-                        return -1;
-                    }
-                    return 0;
-                }));
+                if (xhr.status < 400) {
+                    resolve(xhr.response.sort(function (a, b) {
+                        if (a.name > b.name) {
+                            return 1;
+                        }
+                        if (a.name < b.name) {
+                            return -1;
+                        }
+                        return 0;
+                    }));
+                } else {
+                    reject();
+                }
             });
         }
     );
-}
-
-loadTowns();
+};
 
 /*
  Функция должна проверять встречается ли подстрока chunk в строке full
@@ -69,6 +71,7 @@ loadTowns();
    isMatching('Moscow', 'SCO') // true
    isMatching('Moscow', 'Moscov') // false
  */
+
 function isMatching(full, chunk) {
     if (full.toLowerCase().indexOf(chunk.toLowerCase()) !== -1) {
         return true;
@@ -78,7 +81,6 @@ function isMatching(full, chunk) {
 }
 
 const homeworkContainer = document.querySelector('#homework-container');
-
 /* Блок с надписью "Загрузка" */
 const loadingBlock = homeworkContainer.querySelector('#loading-block');
 /* Блок с текстовым полем и результатом поиска */
@@ -88,20 +90,53 @@ const filterInput = homeworkContainer.querySelector('#filter-input');
 /* Блок с результатами поиска */
 const filterResult = homeworkContainer.querySelector('#filter-result');
 
-let cities = [];
+var townArray = [];
 
 loadTowns()
-    .then(res => {
-        cities = res;
+    .then(response => {
+        townArray = response;
         loadingBlock.style.display = 'none';
         filterBlock.style.display = 'block';
+    })
+    .catch(() => {
+        let reloadButton = document.createElement('button');
+
+        reloadButton.textContent = 'Повторить';
+        reloadButton.addEventListener('click', function (e) {
+            loadTowns().then(response => {
+                townArray = response;
+                loadingBlock.style.display = 'none';
+                filterBlock.style.display = 'block';
+            });
+        });
+
+        homeworkContainer.appendChild(reloadButton);
+        loadingBlock.textContent = 'Не удалось загрузить города';
     });
 
 filterInput.addEventListener('keyup', function () {
-    if(filterInput.value){
-        filterResult.innerHTML = cities
+    if (filterInput.value) {
+        filterResult.innerHTML = townArray
             .filter(item => isMatching(item.name, filterInput.value))
-            .map(item => '<li>' + `${item.name}` + '</li>')
+            .map(function (item) {
+                var deleteButton = document.createElement('button');
+                var townItem = document.createElement('li');
+
+                deleteButton.classList.add('delete-town');
+                townItem.classList.add('town-item');
+
+                deleteButton.textContent = 'Удалить';
+                townItem.textContent = item.name;
+
+                deleteButton.addEventListener('click', function (event) {
+                    console.log('123123123');
+                    filterResult.removeChild(townItem);
+                });
+
+                /*townItem.appendChild(deleteButton);*/
+
+                return townItem.outerHTML;
+            })
             .join('')
     } else {
         filterResult.innerHTML = '';
