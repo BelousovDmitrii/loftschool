6a83df3e952e16e030b4b7b122582915a3bc204b
Dmitrii <40655721+BelousovDmitrii@users.noreply.github.com>
2018-08-19 21:58:01 +1000

    Update cookie.js

--- src/cookie.js
+++ src/cookie.js
@@ -1,43 +1,42 @@
 /*
  ДЗ 7 - Создать редактор cookie с возможностью фильтрации
-
  7.1: На странице должна быть таблица со списком имеющихся cookie. Таблица должна иметь следующие столбцы:
    - имя
    - значение
    - удалить (при нажатии на кнопку, выбранная cookie удаляется из браузера и таблицы)
-
  7.2: На странице должна быть форма для добавления новой cookie. Форма должна содержать следующие поля:
    - имя
    - значение
    - добавить (при нажатии на кнопку, в браузер и таблицу добавляется новая cookie с указанным именем и значением)
-
  Если добавляется cookie с именем уже существующией cookie, то ее значение в браузере и таблице должно быть обновлено
-
  7.3: На странице должно быть текстовое поле для фильтрации cookie
  В таблице должны быть только те cookie, в имени или значении которых, хотя бы частично, есть введенное значение
  Если в поле фильтра пусто, то должны выводиться все доступные cookie
  Если дабавляемая cookie не соответсвуте фильтру, то она должна быть добавлена только в браузер, но не в таблицу
  Если добавляется cookie, с именем уже существующией cookie и ее новое значение не соответствует фильтру,
  то ее значение должно быть обновлено в браузере, а из таблицы cookie должна быть удалена
-
  Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
  */
 
 /*
  homeworkContainer - это контейнер для всех ваших домашних заданий
  Если вы создаете новые html-элементы и добавляете их на страницу, то дабавляйте их только в этот контейнер
-
  Пример:
    const newDiv = document.createElement('div');
    homeworkContainer.appendChild(newDiv);
  */
+
 function getCookieList() {
-    return document.cookie.split('; ').reduce((prev, current) => {
-        const [name, value] = current.split('=');
-        prev[name] = value;
-        return prev
-    }, {})
-};
+    return document.cookie
+        .split('; ')
+        .filter(Boolean)
+        .map(cookie => cookie.match(/^([^=]+)=(.+)/))
+        .reduce((obj, [, name, value]) => {
+            obj[name] = value;
+
+            return obj;
+        }, {});
+}
 
 function setCookie(name, value, options) {
     options = options || {};
@@ -66,29 +65,13 @@ function setCookie(name, value, options) {
     }
 
     document.cookie = updatedCookie;
-};
+}
 
 function isMatching(full, chunk) {
     if (full.toLowerCase().indexOf(chunk.toLowerCase()) !== -1) {
         return true;
-    } else {
-        return false;
     }
-};
-
-function deleteCookie(name) {
-    setCookie(name, '', {
-        expires: -1
-    });
-};
-
-function isMatching(full, chunk) {
-    if (full.toLowerCase().indexOf(chunk.toLowerCase()) !== -1) {
-        return true;
-    } else {
-        return false;
-    }
-};
+}
 
 const homeworkContainer = document.querySelector('#homework-container');
 // текстовое поле с именем cookie
@@ -116,25 +99,17 @@ function getTableWithCookie() {
         }
 
         deleteButton.textContent = 'Удалить';
+        deleteButton.addEventListener('click', function (e) {
+            document.cookie = e.target.parentNode.previousElementSibling.previousElementSibling.textContent + `=;expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+
+            getTableWithCookie();
+        });
 
         tr.innerHTML = '<td>' + item + '</td><td>' + cookieList[item] + '</td><td></td>';
 
         tr.lastElementChild.append(deleteButton);
 
         listTable.append(tr);
-
-        if (addNameInput.value == item && addValueInput.value != filterNameInput.value) {
-            if (filterNameInput.value != '') {
-                tr.outerHTML = '';
-            }
-
-            setCookie(item, cookieList[item]);
-        }
-
-        deleteButton.addEventListener('click', function () {
-            deleteCookie(item);
-            tr.outerHTML = '';
-        });
 
     }
 }
@@ -149,7 +124,4 @@ addButton.addEventListener('click', () => {
     setCookie(addNameInput.value, addValueInput.value);
 
     getTableWithCookie();
-
-    addValueInput.value = '';
-    addNameInput.value = '';
 });
